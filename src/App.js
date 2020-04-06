9865904f5ca12cbe4d4a5cd1bcbb39a49366f05c
freddypaulz <freddypaulz@gmail.com>
2020-04-06 12:44:25 +0530

    changed App.js text

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
                target='_blank'
                rel='noopener noreferrer'
             >
-               CLICK TO START
+               Feature branch
             </a>
          </header>
       </div>
