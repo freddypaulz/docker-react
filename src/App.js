169e2062aed44a658543e08b57d40a32285b7eb8
freddypaulz <freddypaulz@gmail.com>
2020-04-06 12:48:06 +0530

    changed App.js text

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <div className='App'>
          <header className='App-header'>
             <img src={logo} className='App-logo' alt='logo' />
-            <p>Hello</p>
+            <p>Feature branch change</p>
             <a
                className='App-link'
                href='https://reactjs.org'
                target='_blank'
                rel='noopener noreferrer'
             >
-               Feature branch
+               CLICK TO START
             </a>
          </header>
       </div>
